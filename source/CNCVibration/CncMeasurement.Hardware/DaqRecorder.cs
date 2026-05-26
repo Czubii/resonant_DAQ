266aedaedd9d4ffd625057dfca075c7f701817f8
Patryk Januś <patryk.jap@gmail.com>
2026-05-26 03:47:17 +0200

    Created DaqRecorder class

---
+++ source/CNCVibration/CncMeasurement.Hardware/DaqRecorder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace CncMeasurement.Hardware
+{
+    public class DaqRecorder
+    {
+
+    }
+}
