436d6a9101090115d2051d54793c6f69072e0b26
Patryk Januś <patryk.jap@gmail.com>
2026-05-26 13:14:04 +0200

    need to commit before rebasing

--- source/CNCVibration/CncMeasurement.Hardware/DaqRecorder.cs
+++ source/CNCVibration/CncMeasurement.Hardware/DaqRecorder.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using CncMeasurement.Core.models;
+using NationalInstruments.DAQmx;
 
 namespace CncMeasurement.Hardware
 {
