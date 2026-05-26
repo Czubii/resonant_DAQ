0d2186c6c5dc2d7deda788a2bd13d7ffe5e05b07
Patryk Januś <patryk.jap@gmail.com>
2026-05-26 04:05:59 +0200

    Added baseline definition for CaptureBlock method

--- source/CNCVibration/CncMeasurement.Hardware/DaqRecorder.cs
+++ source/CNCVibration/CncMeasurement.Hardware/DaqRecorder.cs
@@ -2,11 +2,23 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using CncMeasurement.Core.models;
 
 namespace CncMeasurement.Hardware
 {
     public class DaqRecorder
     {
+        /// <summary>
+        /// Captures a number of samples froms specified DAQ channel
+        /// The number of samples taken will be n=config.DurationSeconds*config.SampleRate
+        /// </summary>
+        /// <param name="config">Measurement configuration</param>
+        /// <returns></returns>
+        public double[] CaptureBlock(MeasurementConfig config)
+        {
+            int totalSamples = (int)(config.SampleRate * config.DurationSeconds);
 
+            //TODO: Finish implementation, taking into account a proper configuration for the sensor
+        }
     }
 }
