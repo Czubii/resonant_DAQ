33a0251315e0816eb81f3b5b866eb462e031a9fc
Patryk Januś <patryk.jap@gmail.com>
2026-05-27 21:27:32 +0200

    minor changes

---
+++ source/CNCVibration/CncMeasurement.Hardware/TDMSMeasurementWriter.cs
@@ -0,0 +1,37 @@
+﻿using CncMeasurement.Hardware.Acquisition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NationalInstruments.Tdms;
+
+namespace CncMeasurement.Hardware
+{
+    //public interface IMeasurementWriter
+    //{
+    //    Task OpenAsync(AcquisitionConfig config);
+    //    Task WriteAsync(SampleChunk chunk, CancellationToken ct);
+    //    Task CloseAsync();
+    //}
+    //public sealed class TDMSMeasurementWriter : IMeasurementWriter, IDisposable
+    //{
+    //    public Task OpenAsync(AcquisitionConfig config)
+    //    {
+    //        NationalInstruments.Tdms.TdmsFile.
+    //        return;
+    //    }
+    //    public Task WriteAsync(SampleChunk chunk, CancellationToken ct)
+    //    {
+
+    //    }
+    //    public Task CloseAsync()
+    //    {
+
+    //    }
+    //    public void Dispose()
+    //    {
+    //        //TODO: implement
+    //    }
+    //}
+}
