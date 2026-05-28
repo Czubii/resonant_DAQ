4dbeb5b9be9b69120bac4403896967ae0a835d87
Patryk Januś <patryk.jap@gmail.com>
2026-05-28 12:59:08 +0200

    Added a test runner target for testing hardware features

---
+++ source/CNCVibration/TestRunner/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CncMeasurement.Hardware;
+using CncMeasurement.Core;
+using System.Diagnostics.Contracts;
+
+namespace TestRunner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestDiscovery();
+        }
+        static void TestDiscovery()
+        {
+            var testDiscovery = new DaqDiscovery();
+            Console.WriteLine("Avaialbe Devices:");
+            var devices = testDiscovery.GetAvailableDevices();
+
+            foreach (var device in devices)
+            {
+                Console.WriteLine(device.DeviceName);
+                foreach (var ch in device.AIChannels)
+                {
+                    Console.WriteLine($"{ch}");
+                }
+            }
+        }
+    }
+}
