6cf99c130e4b8a410f98475ce5022f3455a8a52b
Patryk Januś <patryk.jap@gmail.com>
2026-05-30 15:49:30 +0200

    Added mock discovery that returns an exemplary list of device description

---
+++ source/CNCVibration/CncMeasurement.MockHardware/MockDaqDiscovery.cs
@@ -0,0 +1,36 @@
+﻿using CncMeasurement.Core.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CncMeasurement.MockHardware
+{
+    public class MockDaqDiscovery : Core.Interfaces.IDaqDiscovery
+    {
+        public List<DeviceDescription> GetAvailableDevices()
+        {
+            var devices = new List<DeviceDescription>();
+
+            string[] deviceNames = ["Dev0", "Dev0Mod0"];
+
+            foreach (string name in deviceNames)
+            {
+
+                var desc = new DeviceDescription
+                {
+                    DeviceName = name,
+                    AIChannels = [$"{name}/ai0", $"{name}/ai1", $"{name}/ai2", $"{name}/ai3"],
+                    ProductType = "Mock Device",
+                    SerialNumber = "Simulated/None"
+                };
+
+                devices.Add(desc);
+            }
+
+            return devices;
+
+        }
+    }
+}
