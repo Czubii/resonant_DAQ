fbc88bde2147a8a109638912fe59f1c6200582d7
Patryk Januś <patryk.jap@gmail.com>
2026-06-01 15:02:57 +0200

    Implemented Damping estimation and integrated it into modal analyzer

--- source/CNCVibration/CncMeasurement.Processing/ModalAnalyzer.cs
+++ source/CNCVibration/CncMeasurement.Processing/ModalAnalyzer.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,6 +18,12 @@ namespace CncMeasurement.Processing
         {
             var fftSpectrum = FFTSpectrum.Compute(signalWindow);
 
+            var dampingRatios = DampingEstimator.Compute(signalWindow, 500, 10);
+
+            for (int ch = 0; ch<signalWindow.NumChannels; ch++)
+            {
+                Console.WriteLine($"Channel {signalWindow.AssignedChannelNames[ch]} | Damping Ratio: {dampingRatios[ch]}");
+            }
 
             return new ModalResults { fft = fftSpectrum };
         }
